38e658ed01b12f20833be06423129f8b709b491c
Ilja Häkkinen <ihk0110@gmail.com>
2017-01-26 23:48:05 +0200

    tietokantataulut ja dokumentaatio

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,9 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO SignedUser (username, password) VALUES ('pekka', 'salasana');
+INSERT INTO SignedUser (username, password) VALUES ('urpo', '1234');
+INSERT INTO Song (name, written_by, year, country, genre) VALUES ('This Is How It Feels', 'Clint Boon', 1999, 'United Kingdom', 'alt. rock, Madchester');
+INSERT INTO Performer (name, active_years, country, genre) VALUES ('Inspiral Carpets', '1983-1995', 'United Kingdom', 'alt. rock, Madchester');
+INSERT INTO Club (name, country, league) VALUES ('Manchester United FC', 'United Kingdom', 'Premier League');
+INSERT INTO Chant (name, lyrics, song) VALUES ('This Is How It Feels to Be City', 'diipa daapa', 1);
+INSERT INTO PerfSong (song_id, perf_id) VALUES (1, 1);
+INSERT INTO ClubChant (chant_id, club_id) VALUES (1, 1);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,49 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE SignedUser(
+    id SERIAL PRIMARY KEY,
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL
+);
+
+CREATE TABLE Song(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    written_by varchar(50),
+    year integer,
+    country varchar(50),
+    genre varchar(50)
+);
+
+CREATE TABLE Performer(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    active_years varchar(9),
+    country varchar(50),
+    genre varchar(50)
+);
+
+CREATE TABLE Club(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    country varchar(50),
+    league varchar(50)
+);
+
+CREATE TABLE Chant(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    lyrics varchar(500),
+    song integer REFERENCES Song(id)
+);
+
+CREATE TABLE PerfSong(
+    id SERIAL PRIMARY KEY,
+    song_id integer REFERENCES Song(id),
+    perf_id integer REFERENCES Performer(id)
+);
+
+CREATE TABLE ClubChant(
+    id SERIAL PRIMARY KEY,
+    chant_id integer REFERENCES Chant(id),
+    club_id integer REFERENCES Club(id)
+);
