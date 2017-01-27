a13f169b639d97e19e1ded867068fcd261d480fc
Ilja Häkkinen <ihk0110@gmail.com>
2017-01-27 04:24:08 +0200

    Pientä fiksausta ja lisää placeholdereita.

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -25,6 +25,42 @@ class HelloWorldController extends BaseController {
         View::make('suunnitelmat/song_edit.html');
     }
 
+    public static function performer_list(){
+        View::make('suunnitelmat/performer_list.html');
+    }
+
+    public static function performer_show() {
+        View::make('suunnitelmat/performer_show.html');
+    }
+
+    public static function performer_edit() {
+        View::make('suunnitelmat/performer_edit.html');
+    }
+
+    public static function chant_list(){
+        View::make('suunnitelmat/chant_list.html');
+    }
+
+    public static function chant_show() {
+        View::make('suunnitelmat/chant_show.html');
+    }
+
+    public static function chant_edit() {
+        View::make('suunnitelmat/chant_edit.html');
+    }
+    
+    public static function club_list(){
+        View::make('suunnitelmat/club_list.html');
+    }
+
+    public static function club_show() {
+        View::make('suunnitelmat/club_show.html');
+    }
+
+    public static function club_edit() {
+        View::make('suunnitelmat/club_edit.html');
+    }
+    
     public static function login() {
         View::make('suunnitelmat/login.html');
     }
--- config/routes.php
+++ config/routes.php
@@ -20,6 +20,42 @@
     HelloWorldController::song_edit();
   });
   
+  $routes->get('/testi/performers', function() {
+    HelloWorldController::performer_list();
+  });
+  
+  $routes->get('/testi/show_performers', function() {
+    HelloWorldController::performer_show();
+  });
+  
+  $routes->get('/testi/edit_performers', function() {
+    HelloWorldController::performer_edit();
+  });
+  
+  $routes->get('/testi/chants', function() {
+    HelloWorldController::chant_list();
+  });
+  
+  $routes->get('/testi/show_chants', function() {
+    HelloWorldController::chant_show();
+  });
+  
+  $routes->get('/testi/edit_chants', function() {
+    HelloWorldController::chant_edit();
+  });
+
+  $routes->get('/testi/clubs', function() {
+    HelloWorldController::club_list();
+  });
+  
+  $routes->get('/testi/show_clubs', function() {
+    HelloWorldController::club_show();
+  });
+  
+  $routes->get('/testi/edit_clubs', function() {
+    HelloWorldController::club_edit();
+  });
+  
   $routes->get('/login', function() {
     HelloWorldController::login();
   });
