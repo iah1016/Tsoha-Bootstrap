7ffaaaa8e30a047afc49f0ac4e0ee3baf5dedd9e
Ilja Häkkinen <ihk0110@gmail.com>
2017-03-05 23:58:13 +0200

    Monesta moneen: lisäämiset ja muokkaamiset kesken.

--- app/controllers/club_controller.php
+++ app/controllers/club_controller.php
@@ -15,14 +15,18 @@ class ClubController extends BaseController {
     public static function create() {
         self::check_logged_in();
 
-        View::make('club/club_new.html');
+        $chants = Chant::all();
+        View::make('club/club_new.html', array('chants' => $chants));
     }
 
     public static function store() {
         self::check_logged_in();
 
         $params = $_POST;
-        $attributes = self::create_attribute_array($params);
+
+        $chants = $params['chants'];
+        $attributes = self::create_attribute_array($params, $chants);
+
         $club = new Club($attributes);
 
         self::try_adding_or_updating($club, $attributes, 'new', 'added');
@@ -32,7 +36,9 @@ class ClubController extends BaseController {
         self::check_logged_in();
 
         $club = Club::find($id);
-        View::make('club/club_edit.html', array('attributes' => $club));
+        $chants = Chant::all();
+        View::make('club/club_edit.html', array(
+            'attributes' => $club, 'chants' => $chants));
     }
 
     public static function update($id) {
@@ -59,8 +65,10 @@ class ClubController extends BaseController {
             Redirect::to('/club/' . $club->id, array(
                 'message' => 'Club ' . $action_string . ' successfully'));
         } else {
+            $chants = Chant::all();
             View::make('club/club_' . $redirect_on_fail . '.html', array(
-                'errors' => $errors, 'attributes' => $attributes));
+                'errors' => $errors, 'attributes' => $attributes,
+                'chants' => $chants));
         }
     }
 
@@ -74,12 +82,27 @@ class ClubController extends BaseController {
             'message' => 'Club removed successfully'));
     }
 
-    private static function create_attribute_array(array $params) {
+    private static function create_attribute_array
+    (array $params, array $chants) {
         return array(
             'name' => $params['name'],
             'country' => $params['country'],
-            'league' => $params['league']
+            'league' => $params['league'],
         );
+    }
+
+    private static function add_chants_to_attributes
+    (array $attributes, array $chants) {
+        $attributes['chants_ids'] = array();
+        if (is_null($chants)) {
+            return $attributes;
+        }
+
+        foreach ($chants as $chant) {
+            $attributes['chants_ids'][] = $chant;
+        }
+
+        return $attributes;
     }
 
 }
